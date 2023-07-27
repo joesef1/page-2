d474c28514c319f3d80d4c5ba8b1c5b2a18bddb1
jousef1 <yousefel560@gmail.com>
2023-07-27 18:22:26 +0800

    page_2

--- main.js
+++ main.js
@@ -24,3 +24,34 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // JavaScript code
+  const menuButton = document.getElementById('user-menu-button');
+  const userMenu = document.getElementById('user-menu');
+
+  // Event listener for the menu button click
+  menuButton.addEventListener('click', () => {
+    const expanded = menuButton.getAttribute('aria-expanded') === 'true';
+    menuButton.setAttribute('aria-expanded', !expanded);
+    userMenu.classList.toggle('hidden'); // Toggle the 'hidden' class to show/hide the menu
+  });
+
+  // Event listener for clicks outside the menu to close it
+  window.addEventListener('click', (event) => {
+    if (!menuButton.contains(event.target) && !userMenu.contains(event.target)) {
+      menuButton.setAttribute('aria-expanded', 'false');
+      userMenu.classList.add('hidden');
+    }
+  });
